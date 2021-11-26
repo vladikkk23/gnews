5b4a1a39d2d0d79bd6891c3b05182cf9f9b0d865
Vlad Movileanu <vladislav.movileanu@ati.utm.md>
2021-11-26 20:09:50 +0200

    Initial commit.

---
+++ GNewsApp/GNewsApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GNewsApp
+//
+//  Created by vladikkk on 26/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
